4dec838ba313488b40390c564c74840d249fa89b
torntrousers@gmail.com <ant.elder@gmail.com>
2019-08-03 13:58:05 +0100

    Update to work with the latest ESP8266/Arduino core

--- Esp8266AWSIoTExample/Esp8266AWSIoTExample.ino
+++ Esp8266AWSIoTExample/Esp8266AWSIoTExample.ino
@@ -3,9 +3,9 @@
  * Simplest possible example (that I could come up with) of using an ESP8266 with AWS IoT.
  * No messing with openssl or spiffs just regular pubsub and certificates in string constants
  * 
- * Note: For this to work you do need to do this:
- *       https://github.com/esp8266/Arduino/issues/3944
- *
+ * This is working as at 3rd Aug 2019 with the current ESP8266 Arduino core release:
+ * SDK:2.2.1(cfd48f3)/Core:2.5.2-56-g403001e3=20502056/lwIP:STABLE-2_1_2_RELEASE/glue:1.1-7-g82abda3/BearSSL:6b9587f
+ * 
  * Author: Anthony Elder 
  * License: Apache License v2
  */
@@ -18,6 +18,8 @@ extern "C" {
 const char* ssid = "<yourSSID>";
 const char* password = "<yourPSWD>";
 
+// Find this awsEndpoint in the AWS Console: Manage - Things, choose your thing
+// choose Interact, its the HTTPS Rest endpoint 
 const char* awsEndpoint = "xxxxxxxxxx.iot.us-west-2.amazonaws.com";
 
 // For the two certificate strings below paste in the text of your AWS 
@@ -78,6 +80,32 @@ const String privatePemKey = \
 "zda4WVWGq8TDnYN/rjvs3KNhkoq0guqcslM29M54TAtO8vBFsHQoQA==";
 //-----END RSA PRIVATE KEY-----
 
+// This is the AWS IoT CA Certificate from: 
+// https://docs.aws.amazon.com/iot/latest/developerguide/managing-device-certs.html#server-authentication
+// This one in here is the 'RSA 2048 bit key: Amazon Root CA 1' which is valid 
+// until January 16, 2038 so unless it gets revoked you can leave this as is:
+const String caPemCrt = \
+//-----BEGIN CERTIFICATE-----
+"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF" \
+"ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6" \
+"b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL" \
+"MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv" \
+"b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj" \
+"ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM" \
+"9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw" \
+"IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6" \
+"VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L" \
+"93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm" \
+"jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC" \
+"AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA" \
+"A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI" \
+"U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs" \
+"N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv" \
+"o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU" \
+"5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy" \
+"rqXRfboQnoZsG4q5WTP468SQvvG5";
+//-----END CERTIFICATE-----
+
 WiFiClientSecure wiFiClient;
 void msgReceived(char* topic, byte* payload, unsigned int len);
 PubSubClient pubSubClient(awsEndpoint, 8883, msgReceived, wiFiClient); 
@@ -91,6 +119,9 @@ void setup() {
   WiFi.waitForConnectResult();
   Serial.print(", WiFi connected, IP address: "); Serial.println(WiFi.localIP());
 
+  // get current time, otherwise certificates are flagged as expired
+  setCurrentTime();
+
   uint8_t binaryCert[certificatePemCrt.length() * 3 / 4];
   int len = b64decode(certificatePemCrt, binaryCert);
   wiFiClient.setCertificate(binaryCert, len);
@@ -98,6 +129,10 @@ void setup() {
   uint8_t binaryPrivate[privatePemKey.length() * 3 / 4];
   len = b64decode(privatePemKey, binaryPrivate);
   wiFiClient.setPrivateKey(binaryPrivate, len);
+
+  uint8_t binaryCA[caPemCrt.length() * 3 / 4];
+  len = b64decode(caPemCrt, binaryCA);
+  wiFiClient.setCACert(binaryCA, len);
 }
 
 unsigned long lastPublish;
@@ -141,4 +176,20 @@ int b64decode(String b64Text, uint8_t* output) {
   base64_init_decodestate(&s);
   int cnt = base64_decode_block(b64Text.c_str(), b64Text.length(), (char*)output, &s);
   return cnt;
+}
+
+void setCurrentTime() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: "); Serial.print(asctime(&timeinfo));
 }
