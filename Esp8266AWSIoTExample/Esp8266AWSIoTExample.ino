e5d7af95d8618550a8223add8261315bf1f7579d
ant <torntrousers@gmail.com>
2021-08-07 23:10:58 +0100

    Update to work with the latest ESP8266 Arduino core 3.0.2

--- Esp8266AWSIoTExample/Esp8266AWSIoTExample.ino
+++ Esp8266AWSIoTExample/Esp8266AWSIoTExample.ino
@@ -3,8 +3,7 @@
  * Simplest possible example (that I could come up with) of using an ESP8266 with AWS IoT.
  * No messing with openssl or spiffs just regular pubsub and certificates in string constants
  * 
- * This is working as at 3rd Aug 2019 with the current ESP8266 Arduino core release:
- * SDK:2.2.1(cfd48f3)/Core:2.5.2-56-g403001e3=20502056/lwIP:STABLE-2_1_2_RELEASE/glue:1.1-7-g82abda3/BearSSL:6b9587f
+ * This is working as at 7rd Aug 2021 with the current ESP8266 Arduino core release 3.0.2
  * 
  * Author: Anthony Elder 
  * License: Apache License v2
@@ -23,88 +22,93 @@ const char* password = "<yourPSWD>";
 const char* awsEndpoint = "xxxxxxxxxx.iot.us-west-2.amazonaws.com";
 
 // For the two certificate strings below paste in the text of your AWS 
-// device certificate and private key, comment out the BEGIN and END 
-// lines, add a quote character at the start of each line and a quote 
-// and backslash at the end of each line:
+// device certificate and private key:
 
 // xxxxxxxxxx-certificate.pem.crt
-const String certificatePemCrt = \
-//-----BEGIN CERTIFICATE-----
-"MIIDWTCCAkGgAwIBAgIUQNkz+5jPnrBkY+0xAK7oIPZSvWwwDQYJKoZIhvcNAQEL" \
-"BQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g" \
-"SW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTE3MTIxNzEwNDMy" \
-"OFoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGAaseAwwTQVdTIElvVCBDZXJ0aWZpY2F0" \
-"ZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAPAG3DPHYLqwhRrp5erJ" \
-"6Nn24IGCGwMKGTU5mF2cBDPnwcTdAA8kX71EuhRBqhNM5zhxmsWj+zAmXfMKKT7L" \
-"pvRfFyca1BwZH2bYBDmJbyITJAnljR6Jh6lPdGPm0vx2uaVYmI2s2pjQeVyKHl5I" \
-"AFJpTGzoVZtxy4EeDmDFhiDrEGNqpzDEBRcNnyEN1fB7uvfSt9CgXMrjH2kFExHU" \
-"CGCQwabWhwrpFmKmXo2gDKf5h221XdC9162kOzCgDcU+kL00cAHD4avYOro0p9W7" \
-"GPwXeMhWieCDTjtIsps/ECd26lLlCzRfdTWqjrmZtKVpFyNXCZ9PLq3TDM/l9y0j" \
-"mI0CAwEAAaNgMF4wHwYDVR0jBBgwFoAU/gnLwuy6sBTHz/XqmgIMARMfU7EwHQYD" \
-"VR0OBBYEFHIbCyR1g2WNGzwNYKUQ1y/63ojCMAwGA1UdEwEB/wQCMAAwDgYDVR0P" \
-"AQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQBFmQZU+iVcFVeQP1HhbzVOOEAC" \
-"qbg4J4N0F1nL3woQBaP6dc1kzB6V4+rhkHec86A+jzqVMvx14Db9ROVNPRJU2Faa" \
-"DIwdLDvJVqrcBUvNsnsZZw+vq+i0foP5AI0vU9TqMgkIPJRSXjrRJrykQyMfyrOt" \
-"zHG7+NMe3qKnOYclMso0hb1vUpyM8T1An5p+dFP8BrBBHLakRLf2wT9b0TMDHoLi" \
-"vyYczV3OLn62Y8ukZPxfBHOKwrrm10bDVAPd2twM9DbIlZnWkZ/L80pztUlMDi96" \
-"9MP9jfHlJLDFu5pLv42p3Lpd+6YWAvs+8R740WVBEHumSEv3JZrPE3W8tJlH"; 
-//-----END CERTIFICATE-----
+static const char certificatePemCrt[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDWjCCAkKgAwIBAgIVAPimTITsxQA/FWxfo81vheBh52YVMA0GCSqGSIb3DQEB
+CwUAME0xSzBJBgNVBAsMQkFtYXpvbiBXZWIgU2VydmljZXMgTz1BbWF6b24uY29t
+IEluYy4gTD1TZWF0dGxlIFNUPVdhc2hpbmd0b24gQz1VUzAeFw0yMTA4MDcxMzE4
+NDdaFw00OTEyMzEyMzU5NTlaMB4xHDAaBgNVBAMME0FXUyBJb1QgQ2VydGlmaWNh
+dGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDGFDfBrbYj9nyKz5dW
+sIXsru1nzka6O6iSHzARqmQK57SiNqKsNxd+ReZXfbmAB7BpNd8TxNPNM06LrbbS
+Qx4J3JNIsRhBSRUBtWyqtnB6M9CxIT2eJQosKqq4Qis/zSFbIihWuCN2pufpuC9t
+J2PKzmkFfibTHFGTl73uTEHKZX0E4AUB9nhzKZee3S+mCgwfytULQw8TRpP2q9/O
+1hdJ28od2FIcjXpkPV0zEi6tbW8TIT6JJafoHIpnoh8+X6BEaI/9aeuxAFhDjhCL
+ApdRgcATSKj/cqAIZ8Yaw5ti6bADlFitinl4sTPShs1KPv1xnJFc4o0AFW07I+VK
+AwzXAgMBAAGjYDBeMB8GA1UdIwQYMBaAFO1GGmLNzzvC2LEr6WAXhsW4OR7QMB0G
+A1UdDgQWBBQCb4eeAho1uL7UVNUHWFiN2cWmFjAMBgNVHRMBAf8EAjAAMA4GA1Ud
+DwEB/wQEAwIHgDANBgkqhkiG9w0BAQsFAAOCAQEAASC8oiLuPxzENYRZV+Nt1WbZ
+XyzajhMJpyuVW1kIjIbvYVUvXWMGXmhFEOe9pPQr89gL/EGxDooM80nTZ2EEO3CL
+7wqESX7GNUlbex9aB3KU9k+WFVaJe70g9e0l1zyU7f5wSGx0skQ2au3Zf2QYxZmt
+6x3/EjAfu4U8PKz1GMBLKqpNl1J3ndt/kmhxh2TtJAIyQBFf11lIw/zETSPfZhQ3
+DhRxmrNJPwUjSyTHwW1VZLpCPuJWxsc7U3mYasQoHoeQfJeZMIYQKfW0h7HajXdW
+1yaW6Ws01lLDzz4i6gf/r7J8MJGUTZopmWnJ/65MO9tSUvV7pBQztGsy/6GPyA==
+-----END CERTIFICATE-----";
+)EOF";
 
 // xxxxxxxxxx-private.pem.key
-const String privatePemKey = \
-//-----BEGIN RSA PRIVATE KEY-----
-"MIIEpAIBAAKCAQEA8AbcM8dgurCFGunl6sno2fbggYIbAwoZNTmYXZwEM+fBxN0A" \
-"DyRfvUS6FEGqE0znOHGaxaP7MCZd8wopPsum9F8XJxrUHBkfZtgEOYlvIhMkCeWN" \
-"HomHqU90Y+bS/Ha5pViYjazamNB5XIoeXkgAUmlMbOhVm3HLgR4OYMWGIOsQY2qn" \
-"MMQFFw2fIQ3V8Hu699K30KBcyuaMeQUTEdQIYJDBptaHCukWYqZejaAMp/mHbbVd" \
-"0L3XraQ7MKANxT6QvTRwAcPhq9g6ujSn1bsY/Bd4yFaJ4INOO0iymz8QJ3bqUuUL" \
-"NF91NaqOuZm0pWkXI1cJn08urdMMz+X3LSOYjQIDAQABAoIBAEQi7meUdZUJfv+A" \
-"3k+kOYGGX3zkHh7iwv0k+Ge6XkcNxyfHRLkHrlXgKKKPcWRBNHIFbdYlEq08dvcD" \
-"L2owDZjZVY04RHYle5CKSci7hty5akWU8FZ23BfoAjZJ1CLu4Qxa9hNhujwfEhR+" \
-"8Nf6wHykf5l66rJRv19L1PELoWO+SgRiAH6KGAZPLvGD5UX0uqIf+UOJNF0xAZll" \
-"9y3bbOL9Ib1GqSIfgUg36CuWQLWVTQQC1JT1e8genu0IniR5DuIEXievktzrb29c" \
-"dY+gemApPfH1+u0iX7tNe5o8b9Ljd82eXmIvn+qKWjv9for+pQrNCKOIGUllvFpG" \
-"HS0PAVECgYEA/k9iUXyknkaJ0SaHwQ9vb7tobbYRTJimqOSVNmyzKIsjexdj4xDz" \
-"aNssQkoAIorfVUFiGPJNJ2fEiJ44BJoXTGteaQIFONiVuykWEENj78ytlBMnqMTt" \
-"qdHRWhmd15/gjDDcSrYtnBkO3l9V0ARydhyf5hamVE6OQCXgcXHI1rcCgYEA8Z8t" \
-"pCGCqXb4+LyQAXqWYs9JRdE1guz5flrw+3iQS+cfYZubSIp9sdOHUKvJIwd/jCYN" \
-"d7sFEkfSgCiDEp7q5Q4I4jryMt0UpLyapNXnZfFU5Lf9xQxuHDcWC/Jo/qKDOyqX" \
-"QWS82uX5sJh3YKGpn/XmJv5iVUKa+JZkiEZxZtsCgYEA6TZbb2bif9MoJ4kyDTXe" \
-"d5RhW9sPWjLTAyc8PsDIO3H9g7AbhevRjGMR32iFXygDTue60i3B21Upwi92J6I3" \
-"o3kDB0E3EL5t8UjoVtm2F/6XX6eBQx/04zrqxbmgKlEPVwCnxEv036WLIRpEU5MA" \
-"dJgjeplixwWTJfiWLvKtv1kCgYEArNdCTfPOxS0fIse4d+ZaZ6qCbtgeYF7mhpXF" \
-"Zw4yoqldkHRAm56+FZQMaw/H3TAC9J1P6wM0uIP6N8VDJfHadCoa/sJN+Uae+s0U" \
-"DrAZTJpbMRXRCcE/jx10vu0UYHqeR5fLBXNriWcTu1kFpHFgptW/Yst8SiNQOl1S" \
-"4w0JGBsCgYBK2fz0rT8UaN4VSQAMa/j/Abx9bsCVSM6RTc095fKzXcLBS/1Q7xb8" \
-"W+6jIbUCFMoHEBbUvWCE/c8W+gxdWcr/ihPrTykM6PFAva6hfS5NRcQop2yv1LOj" \
-"zda4WVWGq8TDnYN/rjvs3KNhkoq0guqcslM29M54TAtO8vBFsHQoQA==";
-//-----END RSA PRIVATE KEY-----
+static const char privatePemKey[] PROGMEM = R"EOF(
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAxhQ3wa22I/Z8is+XVrCF7K7tZ85Gujuokh8wEapkCue0ojai
+rDcXfkXmV325gAewaTXfE8TTzTNOi6220kMeCdyTSLEYQUkVAbVsqrZwejPQsSE9
+niUKLCqquEIrP80hWyIoVrgjdqbn6bgvbSdjys5pBX4m0xxRk5e97kxBymV9BOAF
+AfZ4cymXnt0vpgoMH8rVC0MPE0aT9qvfztYXSdvKHdhSHI16ZD1dMxIurW1vEyE+
+iSWn6ByKZ6IfPl+gRGiP/WnrsQBYQ44QiwKXUYHAE0io/3KgCGfGGsObYumwA5RY
+rYp5eLEz0obNSj79cZyRXOKNABVtOyPlSgMM1wIDAQABAoIBAGlhykQ1mijJ83qa
+dSa+5ZGFg3Lt0L02Djonc5iCcZVHrtd6f9NlwA8DO0gi8HUIIjfnXFaVKiYK/Ofd
+NN3qrWmLu6Q56/CX5CAzmKu+QlsRhB8o0zJ2/FGBJbDAK2hXRz7IDnQMpXlz1QCI
+aFsHUNVOOZudgSRh9Ziza24DjiESKtoDCi2cAy2j95yzQwH/XC6e4IHnsaGuP7sv
+QOdFy4rOtnqZR0fDQe3ToiFdYs3nHqUG8QRDvatoqBYJn0eab2sjGwn5Fk3x3RwI
+b1b8/0kTRvAfA1daJvrlgWp7iXHDerkXqgH1veqwi57FbB73Xp+0yphB+Vu/Csf2
+jjnrPCECgYEA99nQ2z0VA2e4hz9akdAdOR60c3wTZpCI2OyqEaid6dBVF5kaIOhq
+55o5tp6BvvAOFDmimHUwKRP3mCOvyzs1gHrkraAD/u7dwZrwUjz72aKA5WY1JokO
+s4GkrCy+EBbHIRif2K1g26ZuO4Tbod5N3wF7ZsjVIAs26F/Q9cA9DecCgYEAzJd3
+p9+p4N/k4pUSiGqlTXyaxkJN/gH5iXvGOumeh3EB4ecX4p6z83DnxdJDnwz4PLk4
+tO4Aa8vw0g63x30VaWVOH5r9pim5ix/1nKcGnTdHC5+eS6pAfPRGS+h6iPmd4aNU
+RzCHAo9v3XkEPu1Ovfuv0MDwOoKqeUNtJVpYy5ECgYBFx2Igx1KqMcJm+XQzRydj
+FHzzYoRHxPtyLzc2X4WN9WRvj2Am0ah/ahFDk96tdgJQuWVIjP2xIvj7MVjl29aV
+RIQrSbi2RmPShR+R16+EN1cYvqzKG54y9ZX6D2K3PuJzM+6U30M8s9+nDCGVsfO0
+jENEzdgK/H1J8OfwuF24EwKBgAMQyfQz/8Gk2TMj9LEil/N/SHpsB+skcG7E8gVz
+9rIk2VhfNBZrNlsReAeYzPaXJSkTxWvAnTUAS016dvqAP0/i+rayhdZ1jZguFGoS
+fqDgcSoVbDPKC0lUB7GffCaNRvZYkiy25278Yzq4U3315FYobUv9eBwIVTGX9wy2
+O9phAoGAA1UnqYNeeSyD0pxxFm6FiEXPKNxRbusFnyaPK+4kiV0pxcEqgOj0odio
+lCmrSyMMZor3Qxs/E6w4HT5HeIb2QZ475p/GcBvGelELkM+yza1luS9d+O9LgxEd
+4ZQwpSVGjz6212aDaI8R6zVDRRqaJ+gUenEHi05T+rdTfnVE5hg=
+-----END RSA PRIVATE KEY-----
+)EOF";
 
 // This is the AWS IoT CA Certificate from: 
 // https://docs.aws.amazon.com/iot/latest/developerguide/managing-device-certs.html#server-authentication
 // This one in here is the 'RSA 2048 bit key: Amazon Root CA 1' which is valid 
 // until January 16, 2038 so unless it gets revoked you can leave this as is:
-const String caPemCrt = \
-//-----BEGIN CERTIFICATE-----
-"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF" \
-"ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6" \
-"b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL" \
-"MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv" \
-"b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj" \
-"ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM" \
-"9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw" \
-"IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6" \
-"VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L" \
-"93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm" \
-"jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC" \
-"AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA" \
-"A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI" \
-"U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs" \
-"N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv" \
-"o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU" \
-"5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy" \
-"rqXRfboQnoZsG4q5WTP468SQvvG5";
-//-----END CERTIFICATE-----
+static const char caPemCrt[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
+N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
+o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
+5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
+rqXRfboQnoZsG4q5WTP468SQvvG5
+-----END CERTIFICATE-----
+)EOF";
+
+BearSSL::X509List client_crt(certificatePemCrt);
+BearSSL::PrivateKey client_key(privatePemKey);
+BearSSL::X509List rootCert(caPemCrt);
 
 WiFiClientSecure wiFiClient;
 void msgReceived(char* topic, byte* payload, unsigned int len);
@@ -122,17 +126,8 @@ void setup() {
   // get current time, otherwise certificates are flagged as expired
   setCurrentTime();
 
-  uint8_t binaryCert[certificatePemCrt.length() * 3 / 4];
-  int len = b64decode(certificatePemCrt, binaryCert);
-  wiFiClient.setCertificate(binaryCert, len);
-  
-  uint8_t binaryPrivate[privatePemKey.length() * 3 / 4];
-  len = b64decode(privatePemKey, binaryPrivate);
-  wiFiClient.setPrivateKey(binaryPrivate, len);
-
-  uint8_t binaryCA[caPemCrt.length() * 3 / 4];
-  len = b64decode(caPemCrt, binaryCA);
-  wiFiClient.setCACert(binaryCA, len);
+  wiFiClient.setClientRSACert(&client_crt, &client_key);
+  wiFiClient.setTrustAnchors(&rootCert);
 }
 
 unsigned long lastPublish;
